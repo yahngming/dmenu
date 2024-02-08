5172ffbf272fc0451aa4d5cae87085c33f269965
Yang Ming <yahngming@gmail.com>
2024-02-08 19:21:53 +0800

    Update config.h

--- config.h
+++ config.h
@@ -3,12 +3,12 @@
 
 static int topbar = 0;                      /* -b  option; if 0, dmenu appears at bottom     */
 static int centered = 1;                    /* -c  option; if 1, dmenu appears at center     */
-static int min_width = 1000;                /* minimum width when centered                   */
-static const int barspacing = 10;           /* spacing between bar and fonts                 */
+static int min_width = 1024;                /* minimum width when centered                   */
+static const int barspacing = 16;           /* spacing between bar and fonts                 */
 static const unsigned int alpha = 0xdd;     /* Amount of opacity. 0xff is opaque             */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"sans:size=32"
+	"sans-serif:size=32"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
